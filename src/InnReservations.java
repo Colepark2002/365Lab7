cba0330b9b878300a2b486aa07c23780656f6456
Colepark2002 <colepark2002@gmail.com>
2022-12-08 13:30:51 -0800

    set up connections

---
+++ src/InnReservations.java
@@ -0,0 +1,33 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class InnReservations
+{
+    public static void main(String[] args) throws SQLException {
+        InnReservations test = new InnReservations();
+        test.tryConnect();
+    }
+    public void tryConnect() throws SQLException
+    {
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("MySQL JDBC Driver loaded");
+        } catch (ClassNotFoundException ex)
+        {
+            System.err.println("Unable to load JDBC Driver");
+            System.exit(-1);
+        }
+
+        try (Connection conn = DriverManager.getConnection(System.getenv("HP_JDBC_URL"),
+                System.getenv("HP_JDBC_USER"),
+                System.getenv("HP_JDBC_PW")))
+        {
+            System.out.println("Connected");
+        } catch (Exception e)
+        {
+            System.out.println("Did not Connect");
+        }
+    }
+}
