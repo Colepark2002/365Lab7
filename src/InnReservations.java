760ccbc91d8d5e61fa9a655b4b0944dde18b8ee1
Colepark2002 <colepark2002@gmail.com>
2022-12-09 11:34:46 -0800

    Fixed a bug in roomsAndRates() and added start of reservations() method as well as allowing the user to infinitely run the program and quit the program.

--- src/InnReservations.java
+++ src/InnReservations.java
@@ -5,14 +5,41 @@ public class InnReservations
 {
     public static void main(String[] args) throws SQLException {
         InnReservations test = new InnReservations();
-        System.out.println("Rooms and Rates: 1");
-        System.out.println("Reservations: 2");
-        System.out.println("Revenue: 3");
-        System.out.print("Type the number to the right of the colon to access the desired option: ");
-        Scanner sc = new Scanner(System.in);
-        int input = sc.nextInt();
-        System.out.println(""+input);
-        test.roomsAndRates();
+        boolean quit = false;
+        while(!quit)
+        {
+            System.out.println("\n\nQuit: 0");
+            System.out.println("Rooms and Rates: 1");
+            System.out.println("Reservations: 2");
+            System.out.println("Revenue: 3");
+            System.out.print("Type the number to the right of the colon to access the desired option: ");
+            Scanner sc = new Scanner(System.in);
+            int input = sc.nextInt();
+            System.out.println("" + input);
+            switch (input)
+            {
+                case 0:
+                    quit = true;
+                    break;
+
+                case 1:
+                    test.roomsAndRates();
+                    break;
+
+                case 2:
+                    System.out.println("Reservations");
+                    test.reservations();
+                    break;
+
+                case 3:
+                    System.out.println("Revenue");
+                    break;
+
+                default:
+                    System.out.println("Invalid Input Please try again");
+                    break;
+            }
+        }
     }
     public void tryConnect() throws SQLException
     {
@@ -39,11 +66,12 @@ public class InnReservations
 
     public void roomsAndRates() throws SQLException {
         String query = "SELECT RoomCode, RoomName, Beds, bedType, maxOcc, basePrice, decor,"
-                + " SUM(DATEDIFF(res.Checkout, res.CheckIn)) / 180.0 AS RoomPopularityScore,"
+                + " IF(SUM(DATEDIFF(res.Checkout, res.CheckIn)) > 180, 180, SUM(DATEDIFF(res.Checkout, res.CheckIn))) / 180.0 AS RoomPopularityScore,"
                 + " MIN(res.CheckIn) AS NextAvailableCheckIn,"
-                + " MAX(res.Checkout) AS MostRecentCheckout"
+                + " MAX(res.Checkout) AS MostRecentCheckout,"
+                + " DATEDIFF(MAX(res.CheckOut), MAX(res.CheckIn)) as MostRecentCheckoutDays"
                 + " FROM lab7_rooms "
-                + " LEFT JOIN lab7_reservations res"
+                + " JOIN lab7_reservations res"
                 + " ON RoomCode = res.Room"
                 + " AND res.Checkout > NOW() - INTERVAL 180 DAY"
                 + " GROUP BY RoomCode"
@@ -64,12 +92,33 @@ public class InnReservations
             // Print room data
             while (rs.next()) {
                 for (int i = 1; i <= columnCount; i++) {
-                    System.out.print(rs.getString(i) + " ");
+                    System.out.print(rs.getString(i) + ", ");
                 }
                 System.out.println();
             }
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
+    }
+
+    public void reservations() throws SQLException
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter First Name: ");
+        String first = sc.nextLine();
+        System.out.println("Enter Last Name: ");
+        String last = sc.nextLine();
+        System.out.println("Enter RoomCode or Any: ");
+        String roomcode = sc.nextLine();
+        System.out.println("Enter Desired Bed Type: ");
+        String bedtype = sc.nextLine();
+        System.out.println("Enter Begin Date: ");
+        String checkIn = sc.nextLine();
+        System.out.println("Enter End Date: ");
+        String checkOut = sc.nextLine();
+        System.out.println("Enter Number of Children: ");
+        int numChild = sc.nextInt();
+        System.out.println("Enter Number of Adults: ");
+        int numAdult = sc.nextInt();
     }
 }
