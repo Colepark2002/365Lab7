8803c08c5d5427f5ea0dad16c83d9bfe1ac74144
Colepark2002 <colepark2002@gmail.com>
2022-12-08 23:00:25 -0800

    Added Rooms and Rates method to InnReservations

--- src/InnReservations.java
+++ src/InnReservations.java
@@ -1,6 +1,4 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Scanner;
 
 public class InnReservations
@@ -14,7 +12,7 @@ public class InnReservations
         Scanner sc = new Scanner(System.in);
         int input = sc.nextInt();
         System.out.println(""+input);
-        test.tryConnect();
+        test.roomsAndRates();
     }
     public void tryConnect() throws SQLException
     {
@@ -36,6 +34,42 @@ public class InnReservations
         } catch (Exception e)
         {
             System.out.println("Did not Connect");
+        }
+    }
+
+    public void roomsAndRates() throws SQLException {
+        String query = "SELECT RoomCode, RoomName, Beds, bedType, maxOcc, basePrice, decor,"
+                + " SUM(DATEDIFF(res.Checkout, res.CheckIn)) / 180.0 AS RoomPopularityScore,"
+                + " MIN(res.CheckIn) AS NextAvailableCheckIn,"
+                + " MAX(res.Checkout) AS MostRecentCheckout"
+                + " FROM lab7_rooms "
+                + " LEFT JOIN lab7_reservations res"
+                + " ON RoomCode = res.Room"
+                + " AND res.Checkout > NOW() - INTERVAL 180 DAY"
+                + " GROUP BY RoomCode"
+                + " ORDER BY RoomPopularityScore DESC";
+
+        try (Connection conn = DriverManager.getConnection(System.getenv("lab7_JDBC_URL"), System.getenv("lab7_JDBC_USER"), System.getenv("lab7_JDBC_PW"));
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(query)) {
+
+            // Print column names
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int columnCount = rsmd.getColumnCount();
+            for (int i = 1; i <= columnCount; i++) {
+                System.out.print(rsmd.getColumnName(i) + " ");
+            }
+            System.out.println();
+
+            // Print room data
+            while (rs.next()) {
+                for (int i = 1; i <= columnCount; i++) {
+                    System.out.print(rs.getString(i) + " ");
+                }
+                System.out.println();
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
         }
     }
 }
